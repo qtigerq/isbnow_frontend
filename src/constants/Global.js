790701cf23e41b55234e6e083a590b1e1fa13cce
Tiago <chicobg@gmail.com>
2022-03-11 21:31:29 -0300

    URL da KingHost

--- src/constants/Global.js
+++ src/constants/Global.js
@@ -1,4 +1,4 @@
-//const Url = 'http://tiagomelo.kinghost.net:21316'
-const Url = 'http://localhost:3001';
+const Url = 'http://tiagomelo.kinghost.net:21316'
+//const Url = 'http://localhost:3001';
 
 export { Url }
